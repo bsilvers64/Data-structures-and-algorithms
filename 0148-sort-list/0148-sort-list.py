b290b7e04c79f6c7c2d8a8a76d7f64fd834d73c0
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-09-06 10:24:45 +0530

    Time: 305 ms (31.91%), Space: 32.1 MB (99.4%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -10,14 +10,35 @@ class Solution:
         
         left = head
         right = self.mid(head)
-        right.next = tmp
+        tmp = right.next
         right.next = None
         right = tmp
 
-        l1 = sortList(left)
-        l2 = sortList(right)
+        l1 = self.sortList(left)
+        l2 = self.sortList(right)
 
         return self.merge(l1, l2)
 
-    def mid(head):
+    def mid(self, head):
         slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def merge(self, l1, l2):
+        temp = dummy = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        if l1:
+            temp.next = l1
+        elif l2:
+            temp.next = l2
+        
+        return dummy.next
