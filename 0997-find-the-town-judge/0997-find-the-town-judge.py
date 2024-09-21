22c356172c30e94c260e9bcae51e8cccfe307caf
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-09-21 14:26:47 +0530

    Time: 616 ms (24.7%), Space: 21.5 MB (87.97%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,13 +1,14 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        mp = {k:[[], 0] for k in range(1,n+1)}
+        inc = {k:0 for k in range(1,n+1)}
+        outg = {k:0 for k in range(1,n+1)}
 
         for i in trust:
-            mp[i[0]][0].append(i[1])
-            mp[i[1]][1] += 1
+            outg[i[0]] += 1
+            inc[i[1]] += 1
 
-        for key in mp:
-            if mp[key][1] == n-1 and not mp[key][0]: return key
+        for k in range(1,n+1):
+            if inc[k] == n-1 and not outg[k]: return k
 
 
         return -1
