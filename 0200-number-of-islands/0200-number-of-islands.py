2c4a8c6f64099c0dd48ee3c1ae4543558f1eb716
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-09-21 16:50:22 +0530

    Time: 273 ms (28.36%), Space: 24.1 MB (38.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,24 +3,32 @@ class Solution:
         ans = 0
         visited = set()
 
-        def dfs(i, j):
-            if i < 0 or j < 0 or i >= len(grid) or \
-            j >= len(grid[0]) or grid[i][j] == "0" or \
-            (i, j) in visited:
-                return
+        def bfs(i, j):
+            q = collections.deque()
+            q.append((i, j))
 
-            visited.add((i, j))
+            while q:
+                r, c = q.popleft()
+                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                for rd, cd in directions:
+                    nr, nc = r + rd, c + cd
 
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
+                    if (
+                        nr < 0 or nc < 0 or nr >= len(grid) or 
+                        nc >= len(grid[0]) or grid[nr][nc] == "0" or
+                        (nr, nc) in visited
+                    ): 
+                        continue
+
+                    
+                    visited.add((nr, nc))
+                    q.append((nr, nc))
 
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1" and (i, j) not in visited: 
-                    dfs(i, j)
+                    bfs(i, j)
                     ans += 1
 
         return ans            
