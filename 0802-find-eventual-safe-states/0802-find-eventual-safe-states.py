f4225a3095320c94da25ca4e88a77ca69d943e5d
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-09-26 02:02:38 +0530

    Time: 596 ms (10.16%), Space: 23.6 MB (96.35%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,29 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        check = collections.defaultdict(bool)
+
+        def dfs(node):
+            print(node)
+            if not node: return True
+            res = True
+            for neighbor in node:
+                if neighbor in check:
+                    if not check[neighbor]: return False
+                    else: continue
+                else:
+                    check[neighbor] = False
+                    res = res and dfs(graph[neighbor])
+                    check[neighbor] = res
+                    if not res: return res
+            return res
+
+
+        
+        for i in range(len(graph)):
+            if i not in check:
+                check[i] = False
+                check[i] = dfs(graph[i])
+
+        return sorted([i for i in check if check[i]])
+
+        
