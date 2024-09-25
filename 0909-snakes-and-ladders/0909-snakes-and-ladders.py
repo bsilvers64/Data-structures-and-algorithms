19a216afbb241f93477a59b813c2f2bbae2f24fe
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-09-25 09:20:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,36 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        def transform(square):
+            nonlocal n
+            square -= 1
+
+            r = n - (square // n) - 1
+
+            c = square % n
+            if not r & 1: c = n - 1 - c
+            
+            return r, c
+        
+        q = collections.deque()
+        visit = set()
+
+        q.append([1, 0])
+        visit.add(1)
+
+        while q:
+            num, move = q.popleft()
+            for i in range(1, 7):
+                nextSquare = num + i
+                r, c = transform(nextSquare)
+
+                if board[r][c] != -1:
+                    nextSquare = board[r][c]
+
+                if nextSquare == n*n: return move + 1
+
+                if nextSquare not in visit:
+                    visit.add(nextSquare)
+                    q.append([nextSquare, move+1]) 
+        return -1
+
