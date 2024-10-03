c34ab95a7b8e4a8a8493abd4cbc0840e5d0e3dfd
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-10-03 07:25:35 +0530

    Time: 413 ms (74.9%), Space: 18.2 MB (17.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,7 +2,7 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0]: return -1
         M = len(grid)
-        directions = [[1, 0], [1, -1], [-1, 0], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1]]
+        directions = [[1, 0], [1, -1], [-1, 0], [-1, -1], [0, -1], [-1, 1], [0, 1], [1, 1]]
         visited = set()
 
         q = collections.deque()
@@ -16,6 +16,7 @@ class Solution:
                 nr, nc = r + i, c + j
                 if nr < 0 or nc < 0 or nr >= M or nc >= M or grid[nr][nc] or (nr, nc) in visited:
                     continue
+                
                 q.append((nr, nc, length+1))
                 visited.add((nr, nc))
 
