638ca01f1fd2b44a60144a442c3d181f22ca7d6e
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-10-05 11:00:52 +0530

    Time: 196 ms (16.38%), Space: 17.6 MB (34.7%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,45 +1,30 @@
 class Solution:
     def findCircleNum(self, grid: List[List[int]]) -> int:
-        edges = []
+        edges = collections.defaultdict(list)
         N = len(grid)
-        par = [i for i in range(N+1)]
-        rank = [1] * (N+1)
 
         for i in range(N):
-            for j in range(i + 1, N): 
-                if grid[i][j] == 1:
-                    edges.append([i+1, j+1])
-
-        def find(node):
-            n = node
-
-            while (n != par[n]):
-                n = par[n]
-            
-            while (n != node):
-                temp = par[node]
-                par[node] = n
-                node = temp
-
-            return n
+            for j in range(N): 
+                if grid[i][j] == 1 and i != j:
+                    edges[i+1].append(j+1)
         
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-            if p1 == p2: return 0
-            
-            if rank[p1] > rank[p2]:
-                rank[p1] += rank[p2]
-                par[p2] = par[p1]
-            else:
-                rank[p2] += rank[p1]
-                par[p1] = par[p2]
+        for i in range(1, N+1):
+            if i not in edges: edges[i] = set()
 
-            return 1
+        visited = set()
+        def dfs(node):
+            if node in visited: return
+            visited.add(node)
+            for n in edges[node]:
+                if n not in visited: dfs(n)
+
+        print(edges)
+        ans = 0
         
-        ans = N
-        
-        for i, j in edges:
-            if union(i, j): ans -= 1
+        for i in edges:
+            if i not in visited: 
+                dfs(i)
+                ans += 1
 
         return ans
 
