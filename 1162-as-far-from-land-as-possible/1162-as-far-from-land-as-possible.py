34d2ed4e030e46824a4cfde23d5b4e4be7090789
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-10-08 06:47:03 +0530

    Time: 351 ms (95.22%), Space: 17.6 MB (44.92%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,14 +1,12 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
         q = collections.deque()
-        visited = set()
         N = len(grid)
 
         for i in range(N):
             for j in range(N):
                 if grid[i][j]: 
                     q.append([i, j, 0])
-                    visited.add((i, j))
 
         if len(q) == 0 or len(q) == N * N:  
             return -1
@@ -17,12 +15,12 @@ class Solution:
         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         while q:
             r, c, dis = q.popleft()
+            ans = grid[r][c]
             for i, j in directions:
                 nr, nc = r + i, c + j
-                if nc < 0 or nr < 0 or nr >= N or nc >= N or (nr, nc) in visited or grid[nr][nc]:
+                if nc < 0 or nr < 0 or nr >= N or nc >= N or grid[nr][nc] != 0:
                     continue
                 q.append([nr, nc, dis+1])
-                visited.add((nr, nc))
-                ans = dis+1
+                grid[nr][nc] = grid[r][c] + 1
 
-        return ans
+        return ans-1 
