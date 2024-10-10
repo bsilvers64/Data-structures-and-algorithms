9bda0e3ba6556cf23ccc0bed177deb5bb102d147
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-10-10 17:11:01 +0530

    Time: 98 ms (82.58%), Space: 17.7 MB (36.25%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -3,24 +3,32 @@ class Solution:
         visited = set()
         R, C = len(grid), len(grid[0])
 
-        def dfs(r, c):
-            if min(r, c) < 0 or r >= R or c >= C: return False
-            if grid[r][c]: return True
+        def bfs(r, c):
+            q = collections.deque()
+            q.append((r, c))
             visited.add((r, c))
-            ans = True 
             directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-            
-            for i, j in directions:
-                nr, nc = r+i, c+j
-                if (nr, nc) in visited: continue
-                ans &= dfs(nr, nc)
-            return ans
-        
-        res = 0
+            is_closed = True
 
+            while q:
+                row, col = q.popleft()
+
+                for i, j in directions:
+                    nr, nc = row + i, col + j
+                    # If the island touches the boundary, it is not closed
+                    if nr < 0 or nc < 0 or nr >= R or nc >= C:
+                        is_closed = False
+                    elif not grid[nr][nc] and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+
+            return is_closed
+
+        res = 0
         for i in range(R):
             for j in range(C):
                 if not grid[i][j] and (i, j) not in visited:
-                    if dfs(i, j): res += 1
+                    if bfs(i, j):
+                        res += 1
+
         return res
-                    
