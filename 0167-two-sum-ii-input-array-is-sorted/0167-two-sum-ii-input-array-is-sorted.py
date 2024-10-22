0d5b020f31ab65f5b4e963fcce879026880162f0
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-10-23 05:04:55 +0530

    Time: 3 ms (99.05%), Space: 17.7 MB (88.33%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,14 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        mp = collections.defaultdict(int)
-        for i, v in enumerate(numbers):
-            if target - v in mp:
-                return [mp[target-v], i + 1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            total = nums[l] + nums[r]
+            if total == target: return [l+1, r+1]
+            elif total > target:
+                total -= nums[r]
+                r -= 1
             else:
-                mp[v] = i + 1
+                total -= nums[l]
+                l += 1                
         
