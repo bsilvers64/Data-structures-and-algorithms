f3a98f65f920c28180c93541d20b18697a1cd6de
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-03 07:03:43 +0530

    Time: 0 ms (100%), Space: 16.6 MB (40.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = {}
-        def backtrack(res_sum):
-            if res_sum in dp: return dp[res_sum]
-            if res_sum > n: return 0
-            if res_sum == n: return 1
+        dp1, dp2 = 1, 1
+        n -= 1
+        curr = 1
+        while n:
+            curr = dp1 + dp2
+            dp2 = dp1
+            dp1 = curr
+            n -= 1
 
-            dp[res_sum] = backtrack(res_sum+1) + backtrack(res_sum+2)
-            return dp[res_sum]
-
-        return backtrack(0)
+        return curr
