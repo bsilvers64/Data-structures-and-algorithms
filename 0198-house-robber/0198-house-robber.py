6fa658678074bdcd372614f296f571bb3bc229d0
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-07 19:45:40 -0700

    Time: 0 ms (100%), Space: 16.6 MB (23.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        mem = {}
-        temp = 0
-        ans = float('inf')
-        def backtrack(i):
-            if i >= len(nums): return 0
-            if i in mem: return mem[i]
-            include = nums[i] + backtrack(i+2)
-            exclude = backtrack(i+1)
-            mem[i] = max(include, exclude)
-            return mem[i]
-        
-        backtrack(0)
-        return mem[0]
+        if len(nums) == 1: return nums[0]
+        dp0, dp1 = nums[0], max(nums[0], nums[1])
+        if len(nums) == 2: return dp1
+
+        for i in range(2, len(nums)):
+            temp = max(dp1, nums[i] + dp0)
+            dp0 = dp1
+            dp1 = temp
+
+        return dp1
