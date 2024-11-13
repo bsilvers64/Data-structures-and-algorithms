07e9b67ee239c652be9e8c4c19432b09530838ed
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-12 18:34:09 -0700

    Time: 2844 ms (34.34%), Space: 16.7 MB (75.76%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,20 +1,12 @@
-import math
 class Solution:
     def numSquares(self, n: int) -> int:
-        memo = {}
+        dp = [float('inf')] * (n+1)
+        dp[0] = 0
 
-        def backtrack(total):
-            if total > n: return float('inf')
-            if total == n: return 0
-            if total in memo: return memo[total]
-
-            count = float('inf')
-            for i in range(1, math.ceil(math.sqrt(n))+1):
-                i *= i
-                count = min(count, 1 + backtrack(total + i))
-            
-            memo[total] = count
-            return memo[total]
+        for target in range(1, n+1):
+            for i in range(1, n+1):
+                if i*i > target: break
+                dp[target] = min(dp[target], 1 + dp[target - i*i])
+                
         
-        return backtrack(0)
-        
+        return dp[-1]
