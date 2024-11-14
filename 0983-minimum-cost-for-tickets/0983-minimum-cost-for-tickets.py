0c7987a1cefe94b937e3ff35d5487a33b8786cea
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-14 01:43:02 -0700

    Time: 3 ms (80.43%), Space: 16.6 MB (69.74%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,22 +1,16 @@
 class Solution:
-    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        memo = {}
+    def mincostTickets(self, days: List[int], cost: List[int]) -> int:
+        dp = [float('inf')] * (days[-1]+1)
+        travel = set(days)
+        dp[0] = 0
 
-        ans = float('inf')
+        for i in range(1, days[-1]+1):
+            if i in travel:
+                oneday = cost[0] + dp[i-1]
+                sevenday = cost[1] + dp[i-7] if i - 7 >= 0 else cost[1]
+                thtyday = cost[2] + dp[i-30] if i - 30 >= 0 else cost[2]
+                dp[i] = min(oneday, sevenday, thtyday)
+            else:
+                dp[i] = dp[i-1]
 
-        def dfs(i):
-            if i in memo: return memo[i]
-            if i == len(days):
-                return 0
-
-            memo[i] = float('inf')
-
-            for d, c in zip([1, 7, 30], costs):
-                j = i
-                while j < len(days) and days[j] < days[i]+d: j+= 1
-                memo[i] = min(memo[i], c + dfs(j))
-
-            return memo[i]
-        
-        return dfs(0)
-
+        return dp[-1]
