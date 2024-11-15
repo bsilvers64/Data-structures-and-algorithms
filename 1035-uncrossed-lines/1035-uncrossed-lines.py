8acee7fe78d8092304d3ec0e6a5c9bd8b02c0c49
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-15 15:53:46 -0700

    Time: 79 ms (75.43%), Space: 17.2 MB (43.62%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
         M, N = len(nums1), len(nums2)
-        memo = [[-1]*N for i in range(M)]
+        dp = [[0]*(N+1) for i in range(M+1)]
 
-        def dfs(i, j):
+        for i in range(1, M+1):
+            for j in range(1, N+1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[M][N]
+
+"""         def dfs(i, j):
             if i >= M or j >= N: return 0
             if memo[i][j] != -1: return memo[i][j]
 
@@ -14,4 +22,4 @@ class Solution:
 
             return memo[i][j]
 
-        return dfs(0, 0)
+        return dfs(0, 0) """
