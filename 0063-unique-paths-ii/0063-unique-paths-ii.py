a2db0a7b6c66d09421935d7793591320a63863b9
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-19 16:57:27 -0700

    Time: 4 ms (12.06%), Space: 16.7 MB (51.68%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,17 +3,16 @@ class Solution:
         if obs[0][0] == 1 or obs[-1][-1] == 1:
             return 0
         
-        obs[0][0] = 1
-        for i in range(1,len(obs)):
-            obs[i][0] = 1 if obs[i][0] == 0 and obs[i-1][0] == 1 else 0
-        
-        for i in range(1,len(obs[0])):
-            obs[0][i] = 1 if obs[0][i] == 0 and obs[0][i-1] == 1 else 0
+        m, n = len(obs), len(obs[0])
 
-        for i in range(1, len(obs)):
-            for j in range(1, len(obs[0])):
-                if not obs[i][j]: obs[i][j] = obs[i-1][j] + obs[i][j-1]
-                else: obs[i][j] = 0 
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+        dp[0][1] = 1
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if not obs[i-1][j-1]: 
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-        return obs[-1][-1]
+        return dp[-1][-1]
         
