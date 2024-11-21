7e541973847146ee7f71e82610d66b82befc63f6
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-11-20 17:28:05 -0700

    Time: 472 ms (32.45%), Space: 62 MB (28.19%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,13 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = {}
+        n = len(coins)+1
+        dp = [[0 for _ in range(amount+1)] for _ in range((n))]
 
-        def dfs(i, a):
-            if (i, a) in memo: return memo[(i, a)]
-            if i >= len(coins) or a > amount: return 0
-            if a == amount: return 1
+        # because for 0 coins, we can make no amount
+        for i in range(amount+1):
+            dp[0][i] = 0
 
-            memo[(i, a)] = dfs(i, a + coins[i]) + dfs(i+1, a)
-            return memo[(i, a)]
+        # because for amount 0 we have 1 way no matter how many coins we have
+        for i in range(n):
+            dp[i][0] = 1
         
-        return dfs(0, 0)
+        for i in range(1, n):
+            for j in range(1, amount+1):
+                not_choose = dp[i-1][j]
+                choose = 0 if (j-coins[i-1]) < 0 else dp[i][j-coins[i-1]]
+                dp[i][j]  = not_choose + choose
+
+        return dp[-1][-1]
