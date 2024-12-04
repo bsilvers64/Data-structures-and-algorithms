75356b02fa4300c1e16e4d2ac4962c4bbe641d22
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-12-03 20:09:42 -0700

    Time: 115 ms (39.91%), Space: 17.6 MB (13%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,14 +1,17 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
         charcount = [0] * 26
-        used = set()
         for i in s: charcount[ord(i)-ord('a')] += 1
-
         res = 0
-        for i in range(26):
-            if charcount[i] == 0: continue
-            while charcount[i] > 0 and charcount[i] in used:
-                charcount[i] -= 1
-                res += 1
-            used.add(charcount[i])
+
+        charcount.sort()
+
+        for i in range(24, -1, -1):
+            if charcount[i] == 0: break
+
+            if charcount[i] >= charcount[i+1]:
+                prev = charcount[i]
+                charcount[i] = max(0, charcount[i+1]-1)
+                res += prev - charcount[i]
+
         return res
