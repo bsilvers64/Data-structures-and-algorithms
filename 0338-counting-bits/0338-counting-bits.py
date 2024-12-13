eaa4668f5937d4292a6336198a612cab26c21717
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-12-12 21:04:31 -0700

    Time: 7 ms (53.25%), Space: 18.1 MB (17.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,11 +2,11 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         if n == 0: return [0]
         dp = [0] * (n+1)
-        dp[0], dp[1], k = 0, 1, 1
 
-        for i in range(2, n+1):
-            if not (i & (i-1)):
-                k *= 2
-            dp[i] = 1 + dp[i-k]
+        offset = 0
+
+        for i in range(1, n+1):
+            if not (i & (i-1)): offset = i
+            dp[i] = 1 + dp[i-offset]
     
         return dp
