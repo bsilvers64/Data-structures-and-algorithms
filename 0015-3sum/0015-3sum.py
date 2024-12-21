4edd02daea76faeb7fcdf9e5000233be6babbfef
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2024-12-20 17:08:25 -0700

    Time: 451 ms (88.47%), Space: 20.6 MB (16.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+        left = 0
+        N = len(nums)
+        ans = []
+
         nums.sort()
-        ans, N = [], len(nums)
 
-        l = 0
-        while l < N:
-            if l > 0 and nums[l] == nums[l-1]: 
-                l += 1
+        while left < N:
+            if left > 0 and nums[left-1] == nums[left]:
+                left += 1
                 continue
-            if nums[l] > 0: break
-            
-            r = N - 1
-            low = l + 1
-            while low < r:
-                total = nums[l] + nums[low] + nums[r]
-                if total == 0: 
-                    ans.append([nums[l], nums[low], nums[r]])
-                    low += 1
-                    r -= 1
-                    while low < r and nums[low] == nums[low-1]: low += 1
-                    while low < r and nums[r] == nums[r+1]: r -= 1
-                elif total > 0:
-                    r -= 1
-                else:
-                    low += 1
-            l += 1
+            if nums[left] > 0: break
 
+            l, r = left + 1, N-1
+            target = nums[left]
+            while l < r:
+                total = nums[left] + nums[l] + nums[r]
+                if total == 0:
+                    ans.append([nums[left], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]: l += 1
+                    while l < r and nums[r] == nums[r+1]: r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    r -= 1
+
+            left += 1
+        
         return ans
+                
             
