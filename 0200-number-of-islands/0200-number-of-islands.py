075340493d6451f92bd4c66cbdc005ccd965e118
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-01 12:54:35 -0700

    Time: 275 ms (25.31%), Space: 28.4 MB (6.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ans = 0
+        count = 0
+
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        M, N = len(grid), len(grid[0])
+
         visited = set()
-
-        def bfs(i, j):
-            q = collections.deque()
-            q.append((i, j))
-
-            while q:
-                r, c = q.popleft()
-                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-                for rd, cd in directions:
-                    nr, nc = r + rd, c + cd
-
-                    if (
-                        nr < 0 or nc < 0 or nr >= len(grid) or 
-                        nc >= len(grid[0]) or grid[nr][nc] == "0" or
-                        (nr, nc) in visited
-                    ): 
-                        continue
-
-                    
-                    visited.add((nr, nc))
-                    q.append((nr, nc))
-
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i, j) not in visited: 
-                    bfs(i, j)
-                    ans += 1
-
-        return ans            
+        
+        def dfs(i, j):
+            if 0 <= i < M and 0 <= j < N and grid[i][j] == "1" and (i, j) not in visited:
+                visited.add((i, j))
+                for r, c in directions:
+                    dfs(i + r, j + c)
+            else:
+                return
+        
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    dfs(i, j)
+                    count += 1
+        
+        return count
