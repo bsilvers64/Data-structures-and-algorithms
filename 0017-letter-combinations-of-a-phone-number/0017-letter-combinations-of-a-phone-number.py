ead5a28f22ec0bf9d8a5dd5810219fa10f2459d5
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-05 15:07:59 -0700

    Time: 0 ms (100%), Space: 17.8 MB (20.41%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,22 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
-        digitToChar = {
-            "2": "abc", "3": "def", "4": "ghi",
-            "5": "jkl", "6": "mno", "7": "pqrs",
-            "8": "tuv", "9": "wxyz"
+        if not digits: return []
+        phone_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
         }
-        if digits == "": return []
 
-        def backtrack(i, curr):
-            if len(curr) == len(digits): 
-                res.append(curr)
+        res = []
+
+        def dfs(i, s):
+            if i >= len(digits):
+                res.append(s)
                 return
-            
-            for j in range(len(digitToChar[digits[i]])):
-                temp = digitToChar[digits[i]][j]
-                backtrack(i+1, curr + temp)
-
-
-        backtrack(0, "")
+            for character in phone_map[digits[i]]:
+                dfs(i+1, s + character)
+        
+        dfs(0, '')
         return res
+
