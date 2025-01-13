40265d93307204344c3f6a01f7dd611f610d0e32
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-12 18:38:58 -0700

    Time: 0 ms (100%), Space: 17.8 MB (23.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         mem = {}
-        def robMax(i, loot):
-            if i >= len(nums): return loot
-            if (i, loot) in mem: return mem[(i, loot)]
-            mem[(i, loot)] = max(robMax(i+2, loot+nums[i]), robMax(i+1, loot))
-            return mem[(i, loot)]
+        def robMax(i):
+            if i >= len(nums): return 0
+            if i in mem: return mem[i]
+            mem[i] = max(robMax(i+2)+nums[i], robMax(i+1))
+            return mem[i]
 
-        return robMax(0, 0)
+        return robMax(0)
