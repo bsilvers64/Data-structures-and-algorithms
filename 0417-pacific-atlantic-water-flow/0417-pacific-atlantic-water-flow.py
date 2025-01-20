c371d6fb04624f5245dce1a5e4c1b8738cc05514
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-20 16:51:09 -0700

    Time: 32 ms (64.53%), Space: 19.4 MB (39.97%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,32 +1,36 @@
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pac, atl = set(), set()
-        ROW, COL = len(heights), len(heights[0])
+    def pacificAtlantic(self, height: List[List[int]]) -> List[List[int]]:
+        pacific, atlantic = set(), set()
+        M, N = len(height), len(height[0])
 
-        def dfs(r, c, visit, prevHeight):
-            if r < 0 or c < 0 or r >= ROW or c >= COL \
-                or (r, c) in visit or heights[r][c] < prevHeight:
-                return 
-            visit.add((r, c))
-            dfs(r+1, c, visit, heights[r][c])
-            dfs(r-1, c, visit, heights[r][c])
-            dfs(r, c+1, visit, heights[r][c])
-            dfs(r, c-1, visit, heights[r][c])
-            
-
-        for c in range(COL):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROW-1, c, atl, heights[ROW-1][c])
+        def dfs(i, j, prev_height, ocean):
+            if 0 <= i < M and 0 <= j < N and (i, j) not in ocean and height[i][j] >= prev_height:
+                ocean.add((i, j))
+                dfs(i, j+1, height[i][j], ocean)
+                dfs(i+1, j, height[i][j], ocean)
+                dfs(i, j-1, height[i][j], ocean)
+                dfs(i-1, j, height[i][j], ocean)
         
-        for r in range(ROW):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COL-1, atl, heights[r][COL-1])
+        # in the atlantic ocean -
+        for i in range(M):
+            if (i, N-1) not in atlantic:
+                dfs(i, N-1, -1, atlantic)
+        for j in range(N):
+            if (M-1, j) not in atlantic:
+                dfs(M-1, j, -1, atlantic)        
 
+        # in the pacific ocean -
+        for i in range(M):
+            if (i, 0) not in pacific:
+                dfs(i, 0, -1, pacific)
+        for j in range(N):
+            if (0, j) not in pacific:
+                dfs(0, j, -1, pacific)
+        
         ans = []
-
-        for r in range(ROW):
-            for c in range(COL):
-                if (r, c) in atl and (r, c) in pac:
-                    ans.append([r, c])
+        for i in range(M):
+            for j in range(N):
+                if (i, j) in pacific and (i, j) in atlantic:
+                    ans.append([i, j])  
 
         return ans
