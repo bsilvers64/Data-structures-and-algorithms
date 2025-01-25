fdc4c49f825435f793af831d65ba78d84389efb5
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-25 15:39:03 -0700

    Time: 0 ms (100%), Space: 17.9 MB (8.66%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,16 +1,4 @@
 class Solution:
-    def find_min_and_reduce(self, arr):
-        res = float('inf')
-        for i in arr:
-            if i != 0: res = min(res, i) 
-        if res != float('inf'):
-            reduced_arr = [i-res if i != 0 else 0 for i in arr]
-            return reduced_arr
-        else: return None
-
     def minimumOperations(self, nums: List[int]) -> int:
-        operations = 0
-        while nums:
-            nums = self.find_min_and_reduce(nums)
-            operations += 1
-        return operations-1
+        nums = set(nums)
+        return len(nums)-1 if 0 in nums else len(nums)
