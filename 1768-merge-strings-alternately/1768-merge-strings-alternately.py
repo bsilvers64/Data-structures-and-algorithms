2614d1f19e32b095c8eaf8544a72155b4beb91f1
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-27 15:29:26 -0700

    Time: 41 ms (31.93%), Space: 17.7 MB (53.49%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,18 +3,12 @@ class Solution:
         res = []
         i, j = 0, 0
 
-        while i < len(word1) and j < len(word2):
-            res.append(word1[i])
-            res.append(word2[j])
-            i += 1
-            j += 1
-        
-        while i < len(word1):
-            res.append(word1[i])
-            i += 1
-
-        while j < len(word2):
-            res.append(word2[j])
-            j += 1
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                res.append(word1[i])
+                i += 1
+            if j < len(word2):
+                res.append(word2[j])
+                j += 1
 
         return ''.join(res)
