47bccf1ac2d83618688a57a517b1ef0d63bdc406
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-01-30 15:25:09 -0700

    Time: 31 ms (48.26%), Space: 18.9 MB (16.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        gas = 0
-        n = len(nums)
-        for i in range(n):
-            if nums[i] > gas:
-                gas = nums[i]
-            if gas == 0 and i != n-1: return False 
-            gas -= 1
+        jump_range = nums[0]
 
-        return True
+        for i in range(len(nums)):
+            if jump_range < nums[i]:
+                jump_range = nums[i]
+            if i == len(nums)-1: return True
+            elif jump_range == 0: return False
+            jump_range -= 1
