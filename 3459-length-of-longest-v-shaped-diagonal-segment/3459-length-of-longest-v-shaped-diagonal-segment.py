ac585b20e759db306a6f2f3ed0a784b003eb5926
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-01 15:08:01 -0700

    Time: 4213 ms (80.64%), Space: 701.3 MB (15.85%) - LeetHub

--- 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.py
+++ 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.py
@@ -1,37 +1,38 @@
 class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
         
-        memo = {}
-
-        @cache
-        def dfs(r, c, d, turn, order):
-            length = 0
-            # checks for - we are within the grid and following the sequence
-            if ((0 <= r < R) and (0 <= c < C) and
-                ((grid[r][c] == 2 and order) or 
-                (grid[r][c] == 0 and not order))):
-
-                # 2 choices - to go straight or to make a 90-degree turn
-
-                length = max(length, 1 + dfs(r + directions[d][0], c + directions[d][1], d, turn, not order))
-
-                if not turn:
-                    d = (d + 1) % 4
-                    length = max(length, 1 + dfs(r + directions[d][0], c + directions[d][1], d, 1, not order))
-
-
-            return length
-             
-
         R, C = len(grid), len(grid[0])
         directions = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
-        ans = 0
+        memo = {}
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        def dfs(r, c, d, turn, order):
+            if not (0 <= r < R and 0 <= c < C):
+                return 0
+            if not ((grid[r][c] == 2 and order) or (grid[r][c] == 0 and not order)):
+                return 0
+            
+            # Use memoization key
+            key = (r, c, d, turn, order)
+            if key in memo:
+                return memo[key]
+
+            # Move straight
+            length = 1 + dfs(r + directions[d][0], c + directions[d][1], d, turn, not order)
+
+            # Make a single 90-degree turn if not turned already
+            if not turn:
+                new_d = (d + 1) % 4  # Turn 90 degrees clockwise
+                length = max(length, 1 + dfs(r + directions[new_d][0], c + directions[new_d][1], new_d, 1, not order))
+
+            memo[key] = length
+            return length
+
+        ans = 0
+        for i in range(R):
+            for j in range(C):
                 if grid[i][j] == 1:
-                    for index, d in enumerate(directions):
-                        nr, nc = i + d[0], j + d[1]
+                    for index in range(4):
+                        nr, nc = i + directions[index][0], j + directions[index][1]
                         ans = max(ans, 1 + dfs(nr, nc, index, 0, 1))
 
         return ans
