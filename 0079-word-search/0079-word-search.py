fb7d4240a0d059cbf9d48e5f821408c1947cb2f3
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-02 20:24:57 -0700

    Time: 5615 ms (20.67%), Space: 16.7 MB (100%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,20 +1,24 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
+        R, C = len(board), len(board[0])
         visited = set()
-        def dfs(index, i, j):
-            if index == len(word): return True
-            if 0<=i<m and 0<=j<n and board[i][j] == word[index] and (i, j) not in visited:
+        def dfs(i, j, k):
+            if min(i, j) >= 0 and i < R and j < C and (i, j) not in visited and board[i][j] == word[k]:
+                if k == len(word)-1: return True
                 visited.add((i, j))
-                contains = (dfs(index+1, i+1, j) or
-                            dfs(index+1, i, j+1) or
-                            dfs(index+1, i-1, j) or
-                            dfs(index+1, i, j-1))
+                res = (dfs(i, j+1, k+1) or
+                        dfs(i+1, j, k+1) or
+                        dfs(i, j-1, k+1) or
+                        dfs(i-1, j, k+1))
                 visited.remove((i, j))
-                return contains
-        
-        for i in range(m):
-            for j in range(n):
+                return res
+
+        for i in range(R):
+            for j in range(C):
                 if board[i][j] == word[0]:
-                    if dfs(0, i, j): return True
+                    if dfs(i, j, 0): 
+                        print(i, j)
+                        return True
+                        
+        
         return False
