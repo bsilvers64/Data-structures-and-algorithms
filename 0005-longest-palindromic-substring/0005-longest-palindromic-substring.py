349fef9c3a00e801a304b5729ac1502d566edb8f
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-03 14:00:46 -0700

    Time: 315 ms (56.08%), Space: 17.9 MB (46.53%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        i = 0
+        r = 0
         max_len = 0
-        max_len_string = ''
-        while i < len(s):
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > max_len:
-                    max_len = r-l+1
-                    max_len_string = s[l:r+1]
-                l -= 1
-                r += 1
-                print(l, r)
-            l, r = i, i+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > max_len:
-                    max_len = r-l+1
-                    max_len_string = s[l:r+1]
-                l -= 1
-                r += 1
-            
-            i += 1
+        result = s[0]
 
-        return max_len_string
+        while r < len(s):
+            i, j = r-1, r+1
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                if (j-i+1) > max_len:
+                    max_len = j-i+1
+                    result = s[i:j+1]
+                i -= 1
+                j += 1
+            
+            i, j = r-1, r
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                if (j-i+1) > max_len:
+                    max_len = j-i+1
+                    result = s[i:j+1]
+                i -= 1
+                j += 1
+
+            r += 1
+
+        return result
