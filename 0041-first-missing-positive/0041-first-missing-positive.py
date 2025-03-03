703933297dc54a26a937f824c8207f0083e811c1
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-02 19:48:09 -0700

    Time: 85 ms (6.62%), Space: 29.5 MB (33.11%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,30 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        N = len(nums)
+        for i in range(N):
+            if nums[i] < 0: nums[i] = 0
+        
+        # missing number will lie in the range - [1, len(nums) + 1]
+        # so in our array index -i will represent that +ve number i exists
+        # len(nums)-1 index will show if len(nums) number exists
+        # our worst case is that len(nums)+1 number doesn't exists in the array 
+        # which is what we will return in the end
+
+        for i in range(N):
+            # get the real +ve number
+            val = abs(nums[i])
+
+            # if it is in bounds, we will make val-1 index as -ve to denote this val exists
+            if 1 <= val <= N:
+                if nums[val-1] == 0:
+                    nums[val-1] = -N-1
+                elif nums[val-1] > 0:
+                    nums[val-1] *= -1
+        
+        print(nums)
+        # this performs check for numbers 1 to N
+        for i in range(0, N):
+            if nums[i] >= 0: return i+1
+        
+        return N+1
+        
