c3aa1a028ff06632743f5abc9b373fb1896360d2
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-09 19:44:52 -0700

    Time: 0 ms (100%), Space: 17.8 MB (88.31%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,18 +1,29 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        color_to_change = image[sr][sc]
         M, N = len(image), len(image[0])
-        directions = [[0, 1], [-1, 0], [1, 0], [0, -1]]
-        og_color = image[sr][sc]
-        queue = collections.deque()
-        def bfs(i, j):
-            queue.append((sr, sc))
-            image[sr][sc] = color
-            while queue:
-                i, j = queue.popleft()
-                for r, c in directions:
-                    nr, nc = i + r, j + c
-                    if min(nr, nc) < 0 or nr >= M or nc >= N or image[nr][nc] != og_color or image[nr][nc] == color: continue
-                    image[nr][nc] = color
-                    queue.append((nr, nc))
-        bfs(sr, sc)
+
+        visited = set()
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        queue = deque()
+        queue.appendleft((sr, sc))
+        visited.add((sr, sc))
+
+        while queue:
+            for _ in range(len(queue)):
+                r, c = queue.pop() 
+                image[r][c] = color
+
+                for d1, d2 in directions:
+                    nr, nc = r + d1, c + d2
+
+                    if (0 <= nr < M and 0 <= nc < N and 
+                        (nr, nc) not in visited and 
+                        image[nr][nc] == color_to_change):
+
+                        queue.appendleft((nr, nc))
+                        visited.add((nr, nc))
+
+
         return image
