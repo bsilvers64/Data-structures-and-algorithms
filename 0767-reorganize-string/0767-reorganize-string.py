d20976f92aaaf649e671598148992a0f6b271571
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-14 09:02:07 -0700

    Time: 4 ms (19.79%), Space: 17.8 MB (48.31%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,45 +1,36 @@
 from heapq import heappush, heappop
-
 class Solution:
     def reorganizeString(self, s: str) -> str:
         heap = []
+        freq = Counter(s)
 
-        character_count = Counter(s)
+        for char, count in freq.items():
+            heappush(heap, [-count, char])
 
-        # push -> [charcter's frequency, charcter] in the heap
-        for character, count in character_count.items():
-            heappush(heap, [-count, character])
+        result = []
 
-        result_string = []
+        while len(heap) > 1:
+            count1, char1 = heappop(heap)
+            count2, char2 = heappop(heap)
 
-        while len(heap) >= 2:
+            result.append(char1)
+            result.append(char2)
+
+            count1 += 1
+            count2 += 1
+
+            if count1 < 0: heappush(heap, [count1, char1])
+            if count2 < 0: heappush(heap, [count2, char2])
         
-            # extract 2 characters from the heap
-            count1, character1 = heappop(heap)
-            count2, character2 = heappop(heap)
 
-            # append these characters one-by-one
-            result_string.append(character1)
-            result_string.append(character2)
-
-            # after using these characters check their count, and if its still left,
-            # push to heap with count decreased by 1
-            if count1 + 1 < 0:
-                heappush(heap, [count1 + 1, character1])
-            
-            if count2 + 1 < 0:
-                heappush(heap, [count2 + 1, character2])
-
-
-
-        # if the remaining character's count is more than 1
-        # OR it is same as last appended charcter to result string, our string is not possible
         if heap:
-            if heap[0][0] < -1 or (result_string and result_string[-1] == heap[0][1]): 
-                return ""
+            if heap[0][0] < -1: return ""
+            else:
+                result.append(heappop(heap)[1])
+        
+        
+        return "".join(result)
 
-            result_string.append(heappop(heap)[1])
 
-        return ''.join(result_string)
 
 
