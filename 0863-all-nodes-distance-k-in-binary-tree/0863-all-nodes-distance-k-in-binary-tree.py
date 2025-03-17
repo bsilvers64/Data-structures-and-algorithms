442019e9fb364b19f8f0e79c0f1510d453f7eb29
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-17 15:09:10 -0700

    Time: 39 ms (76.38%), Space: 18.3 MB (11.93%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,37 +7,48 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        queue = deque()
+        if k == 0: return [target.val]
         graph = defaultdict(list)
 
+        queue = deque()
         queue.appendleft(root)
 
         while queue:
-            node = queue.pop()
-            if node.left:
-                graph[node.val].append(node.left.val)
-                graph[node.left.val].append(node.val)
-                queue.appendleft(node.left)
-            if node.right:
-                graph[node.val].append(node.right.val)
-                graph[node.right.val].append(node.val)
-                queue.appendleft(node.right)
+            for _ in range(len(queue)):
+                parent = queue.pop()
+                if parent.left:
+                    queue.append(parent.left)
+                    graph[parent].append(parent.left)
+                    graph[parent.left].append(parent)
+                if parent.right:
+                    queue.append(parent.right)
+                    graph[parent].append(parent.right)
+                    graph[parent.right].append(parent)
         
-        queue = deque()
-        visited = set()
-        k_away = []
 
-        queue.append([target.val, 0])
+
+        visited = set()
+        visited.add(target.val)
+        queue.appendleft(target)
+        result = []
 
         while queue:
-            level_size = len(queue)
-            for _ in range(level_size):
-                node, distance_to_node = queue.pop()
-                if distance_to_node == k: k_away.append(node)
-                visited.add(node)
+            k -= 1
+            result = []
+            for _ in range(len(queue)):
+                node = queue.pop()
                 for neighbor in graph[node]:
-                    if neighbor not in visited:
-                        queue.appendleft([neighbor, distance_to_node + 1])
+                    if neighbor.val not in visited:
+                        visited.add(neighbor.val)
+                        queue.appendleft(neighbor)
+                        result.append(neighbor.val)
+            if k == 0:
+                break
+        
+        return result
 
-                
-        return k_away
+
+
+
+        
+        return result
