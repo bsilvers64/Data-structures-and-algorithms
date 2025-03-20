95a7121e105b602ba9a6bfaff8d216188be1669c
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-20 15:31:46 -0700

    Time: 0 ms (100%), Space: 17.8 MB (94.28%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,31 +1,30 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(list)
+        graph = defaultdict(list)
+
         k = 0
-        for i, j in equations:
-            adj[i].append([j, values[k]])
-            adj[j].append([i, 1/values[k]])
+        for l1, l2 in equations:   
+            graph[l1].append([l2, values[k]])
+            graph[l2].append([l1, (1/values[k])])
             k += 1
         
-        def bfs(start, dest):
-            if start not in adj or dest not in adj: return -1
-            if start == dest: return 1
+        def bfs(src, dest):
+            if src not in graph or dest not in graph: return -1
+            if src == dest: return 1
 
-            q = collections.deque()
-            v = set()
-            v.add(start)
-            q.append([start, 1])
-            ans = 0
+            q = deque()
+            visit = set()
+            q.appendleft([src, 1])
+            visit.add(src)
 
             while q:
-                n, val = q.popleft()
-                if n == dest: return val
-                for nei, div in adj[n]:
-                    
-                    if nei not in v: 
-                        q.append([nei, div*val])
-                        v.add(nei)
+                node, val = q.pop()
+                if node == dest: return val
+                for neighbor, d in graph[node]:
+                    if neighbor not in visit:
+                        visit.add(neighbor)
+                        q.appendleft([neighbor, val * d])
 
             return -1
-            
-        return [bfs(q[0], q[1]) for q in queries]
+
+        return [bfs(src, dst) for src, dst in queries]
