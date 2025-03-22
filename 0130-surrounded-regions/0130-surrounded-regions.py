273fddcc05fb19bf55cebdc22baf3b634864189a
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-22 10:14:11 -0700

    Time: 3 ms (88.5%), Space: 21.7 MB (58.05%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,30 +3,28 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        ROW, COL = len(board), len(board[0])
-    
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= ROW or c >= COL or board[r][c] != "O":
-                return
-            board[r][c] = "T"
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-
-        for r in range(ROW):
-            for c in range(COL):
-                if r in [0, ROW-1] or c in [0, COL-1]:
-                    dfs(r, c)
-
-
-        for r in range(ROW):
-            for c in range(COL):
-                if board[r][c] == "O": board[r][c] = "X"
-
-
-        for r in range(ROW):
-            for c in range(COL):
-                if board[r][c] == "T": board[r][c] = "O"
-
+        M, N = len(board), len(board[0])
+        def dfs(i, j):
+            if 0 <= i < M and 0 <= j < N and board[i][j] == "O":
+                board[i][j] = "T"
+                dfs(i, j+1)
+                dfs(i+1, j)
+                dfs(i, j-1)
+                dfs(i-1, j)
         
+        for i in range(M):
+            if board[i][0] == "O": dfs(i, 0)
+            if board[i][N-1] == "O": dfs(i, N-1)
+
+        for i in range(N):
+            if board[0][i] == "O": dfs(0, i)
+            if board[M-1][i] == "O": dfs(M-1, i)
+
+        for i in range(M):
+            for j in range(N):
+                if board[i][j] == "O": board[i][j] = "X" 
+
+        for i in range(M):
+            for j in range(N):
+                if board[i][j] == "T": board[i][j] = "O" 
+    
