a50b1a269eb81c7ad11228d646554445926f65d5
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-25 20:43:55 -0700

    Time: 3 ms (44.73%), Space: 18.1 MB (13.08%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,27 +1,16 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        num1 = 0
-        num2 = 0
+        a, b = a[::-1], b[::-1]
+        res = []
+        carry = 0
 
-        n = len(a)-1
-        for i in a:
-            if i == '1':
-                num1 += pow(2, n)
-            n-=1
-
-        n = len(b)-1
-        for i in b:
-            if i == '1':
-                num2 += pow(2, n)
-            n-=1
+        for i in range(max(len(a), len(b))):
+            digit_a = int(a[i]) if i < len(a) else 0
+            digit_b = int(b[i]) if i < len(b) else 0
+            total = digit_a + digit_b + carry
+            carry = total // 2
+            total = total % 2
+            res.append(str(total))
         
-        num = num1+num2
-        if not num: return "0"
-        binary_string = ""
-    
-        while num > 0:
-            remainder = num % 2  # Get the remainder (either 0 or 1)
-            binary_string = str(remainder) + binary_string  # Add remainder to the front
-            num = num // 2  # Perform integer division by 2
-        
-        return binary_string
+        if carry: res.append(str(carry))
+        return "".join(reversed(res))
