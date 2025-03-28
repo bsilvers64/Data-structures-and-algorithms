d6f280a72f543cb4884869d12841d0f4cf234951
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-03-28 13:13:02 -0700

    Time: 32 ms (71.12%), Space: 20 MB (79.35%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,48 +1,26 @@
-class Node:
-    def __init__(self, val=0):
-        self.val = val
-        self.prev = None
-        self.next = None
-
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        node = Node(homepage)
-        self.right, self.left = Node(), Node()
-        node.prev, node.next = self.left, self.right
-        self.right.prev = node
-        self.left.next = node
-        self.curr = node
+        self.i = 0
+        self.len = 1
+        self.history = [homepage]
 
     def visit(self, url: str) -> None:
-        newNode = Node(url)
-        newNode.next = self.right
-        newNode.prev = self.curr
-        self.curr.next = newNode
-        self.curr = newNode
-        self.right.prev = newNode
+        if len(self.history) < self.i+2:
+            self.history.append(url)
+        else:
+            self.history[self.i+1] = url
+        self.i += 1
+        self.len = self.i + 1
+
 
     def back(self, steps: int) -> str:
-        curr = self.curr
-        while curr and steps > 0:
-            curr = curr.prev
-            steps -= 1
-        if not curr or steps < 0 or curr == self.left:
-            curr = self.left.next
-
-        self.curr = curr
-        return curr.val 
+        self.i = max(0, self.i-steps)
+        return self.history[self.i]
 
     def forward(self, steps: int) -> str:
-        curr = self.curr
-        while curr and steps > 0:
-            curr = curr.next
-            steps -= 1
-        if not curr or steps < 0 or curr == self.right:
-            curr = self.right.prev
-
-        self.curr = curr
-        return curr.val         
+        self.i = min(self.len-1, self.i+steps)
+        return self.history[self.i]       
 
 
 # Your BrowserHistory object will be instantiated and called as such:
