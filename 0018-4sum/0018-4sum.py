989ba516de9116f90691eecd911992858c90539e
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-02 15:37:59 -0700

    Time: 391 ms (55.68%), Space: 17.8 MB (58.69%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,28 +1,26 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        answer = []
-        n = len(nums)
+        result = []
+        N = len(nums)
         nums.sort()
 
-        for i in range(n):
+        for i in range(0, N):
             if i > 0 and nums[i] == nums[i-1]: continue
-
-            for j in range(i+1, n):
+            for j in range(i+1, N):
                 if j > i+1 and nums[j] == nums[j-1]: continue
-
-                l, r = j + 1, n-1
+                l, r = j + 1, N-1
                 while l < r:
-                    cur_sum = nums[i] + nums[j] + nums[l] + nums[r]
-
-                    if cur_sum == target:
-                        answer.append([nums[i], nums[j], nums[l], nums[r]])
+                    total = nums[i] + nums[j] + nums[l] + nums[r]
+                    
+                    if target == total: 
+                        result.append([nums[i], nums[j], nums[l], nums[r]])
                         l += 1
                         r -= 1
                         while l < r and nums[l] == nums[l-1]: l += 1
                         while l < r and nums[r] == nums[r+1]: r -= 1
-                    elif cur_sum > target: r -= 1
-                    else: l += 1
-                    
+                    elif total > target:
+                        r -= 1
+                    else:
+                        l += 1
 
-
-        return answer
+        return result
