147832775c368cb7582ac79007a84a7a834bc7a2
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-01 17:48:50 -0700

    Time: 0 ms (100%), Space: 18 MB (23.05%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,9 +5,10 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left, right = ListNode(), ListNode()
+        dummyL = left
+        dummyR = right
         curr = head
-        left = ans = ListNode()
-        right = right_temp = ListNode()
         while curr:
             if curr.val < x:
                 left.next = curr
@@ -16,8 +17,7 @@ class Solution:
                 right.next = curr
                 right = curr
             curr = curr.next
-
-        left.next = right_temp.next
+        
         right.next = None
-
-        return ans.next
+        left.next = dummyR.next
+        return dummyL.next
