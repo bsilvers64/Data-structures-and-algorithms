6251f12294db726779f6e1c2524fb7ec53f8ec5b
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-03 17:19:41 -0700

    Time: 0 ms (100%), Space: 18.1 MB (26.05%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,29 +1,16 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        color_to_change = image[sr][sc]
         M, N = len(image), len(image[0])
+        og_color = image[sr][sc]
+        if color == og_color: return image
 
-        visited = set()
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        
-        queue = deque()
-        queue.appendleft((sr, sc))
-        visited.add((sr, sc))
-
-        while queue:
-            for _ in range(len(queue)):
-                r, c = queue.pop() 
+        def dfs(r, c):
+            if 0 <= r < M and 0 <= c < N and image[r][c] == og_color:
                 image[r][c] = color
-
-                for d1, d2 in directions:
-                    nr, nc = r + d1, c + d2
-
-                    if (0 <= nr < M and 0 <= nc < N and 
-                        (nr, nc) not in visited and 
-                        image[nr][nc] == color_to_change):
-
-                        queue.appendleft((nr, nc))
-                        visited.add((nr, nc))
-
-
+                dfs(r + 1, c)
+                dfs(r, c + 1)
+                dfs(r - 1, c)
+                dfs(r, c - 1)
+        
+        dfs(sr, sc)
         return image
