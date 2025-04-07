abbcc2cc102760fc944d3a8d3de8f1c00f912413
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-06 21:55:59 -0700

    Time: 264 ms (11.07%), Space: 81.3 MB (9.39%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -1,43 +1,48 @@
 class ListNode:
     def __init__(self, val=None, prev=None, next=None):
-        self.prev = prev
-        self.next = next
         self.val = val
+        self.next = next
+        self.prev = prev
 
 class LinkedList:
     def __init__(self):
-        self.map = {}
-        self.left = ListNode(0)
-        self.right = ListNode(0, prev=self.left)
-        self.left.next = self.right
+        self.map = {} # maps ListNode value to the node in the Linked-list
+        self.right = ListNode(0)
+        self.left = ListNode(0, next=self.right)
+        self.right.prev = self.left
 
     def length(self):
+        """ returns the current length of the linked-list """
         return len(self.map)
 
-    # create a new node in the LL and push it on to the rightmost
     def push_right(self, val):
-        node = ListNode(val, self.right.prev, self.right)
+        """ creates a new node and inserts it to the right most of the linked-list 
+            making it as Most-Recently-Used node """
+        
+        node = ListNode(val, prev = self.right.prev, next = self.right)
         self.map[val] = node
-        self.right.prev = node
-        node.prev.next = node
+        next, prev = self.right, self.right.prev
+        prev.next = node
+        next.prev = node
     
-    # delete a node from the LL and from map 
     def pop(self, val):
+        """ removes a node from the map as well as the linked-list """
         if val in self.map:
             node = self.map[val]
             next, prev = node.next, node.prev
-            next.prev = prev
             prev.next = next
+            next.prev = prev
             self.map.pop(val, None)
     
-    # pop the least recently used
     def pop_left(self):
+        """" removes the least recently used node - from the left most 
+            and returns the value """
         res = self.left.next.val
         self.pop(res)
         return res
     
-    # make a val as most recently used in the LL
     def update(self, val):
+        """ making any value node as the most recently used inthe linked-list"""
         self.pop(val)
         self.push_right(val)
 
@@ -45,42 +50,55 @@ class LinkedList:
 class LFUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.lfu_cnt = 0
-        self.val_map = {} # key -> val
-        self.count_map = defaultdict(int) # key -> count
-        self.list_map = defaultdict(LinkedList) # count of key -> LinkedList
-    
+        self.lfu_count = 0
+        self.value_map = {}                     # maps key to value
+        self.count_map = defaultdict(int)       # maps key to a count
+        self.list_map = defaultdict(LinkedList)   # maps count to its LinkedList
+
     def counter(self, key):
-        # get current count of key and increment it 
-        cnt = self.count_map[key]
+        """ updates the count of any key, removes that key from the linked list
+            of that count, then increments the count and inserts this key to the
+            linked-list of that new count """
+
+        # get the current count of this key
+        count = self.count_map[key]
+        
+        # increment the count of this key
         self.count_map[key] += 1
 
-        # pop this key from the previous count's linkedinlist and insert it to new one as MRU 
-        self.list_map[cnt].pop(key)
-        self.list_map[cnt+1].push_right(key)
+        # remove it from the linked list of previous count
+        self.list_map[count].pop(key)
 
-        # update the lfu_count
-        # if there is no node left in that count's LL, then we make LFU LL as next count
-        if cnt == self.lfu_cnt and self.list_map[cnt].length() == 0:
-            self.lfu_cnt += 1
+        # add it to the linkedlist of new count
+        self.list_map[count + 1].push_right(key)
+
+        # update the LFU count if previous count was the LFU count and its 
+        # linked list became empty now
+        if self.lfu_count == count and self.list_map[count].length() == 0:
+            self.lfu_count += 1
+
 
     def get(self, key: int) -> int:
-        if key in self.val_map: 
+        """ gets the key's value if it exists and increments its frequency count """
+        if key in self.value_map:
             self.counter(key)
-            return self.val_map[key]
-        else: return -1
+            return self.value_map[key]
+        else: 
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        if self.cap == 0: return 
+        
+        """ inserts the new key to its count's linkedlist and evicts the least frequently
+            used key (and the least recently used) key in case capacity reached"""
 
-        if self.cap == len(self.val_map) and key not in self.val_map:
-            res = self.list_map[self.lfu_cnt].pop_left()
-            self.val_map.pop(res)
+        if self.cap == len(self.value_map) and key not in self.value_map:
+            res = self.list_map[self.lfu_count].pop_left()
+            self.value_map.pop(res)
             self.count_map.pop(res)
 
-        self.val_map[key] = value
+        self.value_map[key] = value
         self.counter(key)
-        self.lfu_cnt = min(self.lfu_cnt, self.count_map[key])
+        self.lfu_count = min(self.lfu_count, self.count_map[key])
 
 
 # Your LFUCache object will be instantiated and called as such:
