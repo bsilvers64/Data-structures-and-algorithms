4ae40583f8b2507b6484ffe924a25380cadc8a4d
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-13 21:36:07 -0700

    Time: 108 ms (64.81%), Space: 50.4 MB (9.87%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,32 +1,30 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        result = []
         N = len(s)
-        
-        prefixSum = [0] * N
-        nextPlate = [-1] * N
-        previousPlate = [-1] * N
+        plateSum = [0] * N
+        plateCount = 0
 
-        count = 0
-        lastPlateIndex1, lastPlateIndex2 = -1, -1
+        nextCandle, previousCandle = [-1] * N, [-1] * N
+        rightMostCandle, leftMostCandle = -1, -1
 
         for i in range(N):
-            if s[i] == "|": lastPlateIndex1 = i
-            previousPlate[i] = lastPlateIndex1
+            if s[i] == "*": 
+                plateCount += 1
+            elif s[i] == "|":
+                leftMostCandle = i
+            if s[N-i-1] == "|":
+                rightMostCandle = N-i-1
 
-            if s[N-i-1] == "|": lastPlateIndex2 = N-i-1
-            nextPlate[N-i-1] = lastPlateIndex2
+            previousCandle[i] = leftMostCandle
+            nextCandle[N-i-1] = rightMostCandle
+            plateSum[i] = plateCount
 
-            if s[i] == "*": count += 1
-            prefixSum[i] = count
-
-        for query in queries:
-            start = query[0]
-            end = query[1]
-
-            firstPlate = nextPlate[start]
-            lastPlate = previousPlate[end]
-            
-            result.append(prefixSum[lastPlate] - prefixSum[firstPlate]) if (firstPlate < lastPlate and start!=end) else result.append(0)
+        result = []
+        for start, end in queries:
+            if start != end and nextCandle[start] < previousCandle[end]:
+                result.append(plateSum[previousCandle[end]] - plateSum[nextCandle[start]])
+            else:
+                result.append(0)
 
         return result
+            
