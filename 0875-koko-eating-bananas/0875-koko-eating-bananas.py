0a74a1cd73ce81e24ed4d395e4d1b211ea8c6050
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-13 20:59:10 -0700

    Time: 136 ms (94.8%), Space: 18.8 MB (93.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,21 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def k_works(k):
-            hours = h
+        N = len(piles)
+        def check(k):
+            hours = 0
             for bananas in piles:
-                hours -= ceil(bananas/k)
-                if hours < 0: return False
-            return hours >= 0
+                hours += ceil(bananas/k)
+            return hours <= h
 
-        min_k = 0
-        l, r = 1, max(piles)    
+        l, r = 1, max(piles)
 
-        while l <= r:
-            mid = (l + r) // 2
-            if k_works(mid): 
-                min_k = mid
-                r = mid - 1
+        k = r
+        while l < r:
+            mid = (l+r)//2
+            if check(mid):
+                k = mid
+                r = mid 
             else:
                 l = mid + 1
 
-        return min_k
+        return k
