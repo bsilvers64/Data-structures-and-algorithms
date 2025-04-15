c636b2e47cfac9f147ef91e43b6186483af2958e
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-14 17:30:12 -0700

    Time: 0 ms (100%), Space: 17.9 MB (29.03%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,20 +1,22 @@
 from heapq import heappush, heappop
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        count = defaultdict(int)
+        for char in s:
+            count[char] += 1
+
         heap = []
-        freq = Counter(s)
+        for char, freq in count.items():
+            heappush(heap, [-freq, char])
 
-        for char, count in freq.items():
-            heappush(heap, [-count, char])
+        result_string = []
 
-        result = []
-
-        while len(heap) > 1:
+        while len(heap) >= 2:
             count1, char1 = heappop(heap)
             count2, char2 = heappop(heap)
 
-            result.append(char1)
-            result.append(char2)
+            result_string.append(char1)
+            result_string.append(char2)
 
             count1 += 1
             count2 += 1
@@ -22,15 +24,12 @@ class Solution:
             if count1 < 0: heappush(heap, [count1, char1])
             if count2 < 0: heappush(heap, [count2, char2])
         
-
         if heap:
-            if heap[0][0] < -1: return ""
+            if -heap[0][0] > 1 or (result_string and heap[0][1] == result_string[-1]): return ""
             else:
-                result.append(heappop(heap)[1])
+                result_string.append(heappop(heap)[1])
         
-        
-        return "".join(result)
-
-
+        return "".join(result_string)
+            
 
 
