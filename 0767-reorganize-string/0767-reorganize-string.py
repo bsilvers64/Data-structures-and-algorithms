e590cd0c226e7e261d2da76203db61431c815c40
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-14 18:02:45 -0700

    Time: 0 ms (100%), Space: 17.6 MB (93.92%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,35 +1,33 @@
-from heapq import heappush, heappop
 class Solution:
     def reorganizeString(self, s: str) -> str:
         count = defaultdict(int)
-        for char in s:
-            count[char] += 1
+        for c in s:
+            count[c] += 1
+        
+        max_letter, max_count = '', 0
+        for c, freq in count.items():
+            if freq > max_count:
+                max_letter = c
+                max_count = freq
+        
 
-        heap = []
+        if max_count > ((len(s)+1)//2): return ""
+
+        result = [''] * len(s)
+
+        index = 0
+
+        while count[max_letter] != 0:
+            result[index] = max_letter
+            count[max_letter] -= 1
+            index += 2
+
+
         for char, freq in count.items():
-            heappush(heap, [-freq, char])
+            while freq:
+                if index >= len(s): index = 1
+                result[index] = char
+                freq -= 1
+                index += 2
 
-        result_string = []
-
-        while len(heap) >= 2:
-            count1, char1 = heappop(heap)
-            count2, char2 = heappop(heap)
-
-            result_string.append(char1)
-            result_string.append(char2)
-
-            count1 += 1
-            count2 += 1
-
-            if count1 < 0: heappush(heap, [count1, char1])
-            if count2 < 0: heappush(heap, [count2, char2])
-        
-        if heap:
-            if -heap[0][0] > 1 or (result_string and heap[0][1] == result_string[-1]): return ""
-            else:
-                result_string.append(heappop(heap)[1])
-        
-        return "".join(result_string)
-            
-
-
+        return "".join(result) 
