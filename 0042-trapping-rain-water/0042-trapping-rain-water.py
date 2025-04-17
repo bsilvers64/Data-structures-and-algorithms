cf99b7b05cdd92c2662faad08f21470849d18f12
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-17 15:57:17 -0700

    Time: 19 ms (43.39%), Space: 19.3 MB (69.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,19 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        left_max, right_max = nums[0], nums[-1]
 
-        N = len(height)
-        left_max_wall, right_max_wall = [0] * N, [0]* N
-        l_wall, r_wall = 0, 0
-
-        for i in range(N):
-            left_max_wall[i] = l_wall
-            l_wall = max(l_wall, height[i])
-        
-        for i in range(N-1, -1, -1):
-            right_max_wall[i] = r_wall
-            r_wall = max(r_wall, height[i])
-        
         water_collected = 0
 
-        for i in range(N):
-            potential_water = min(left_max_wall[i], right_max_wall[i]) - height[i]
-            potential_water = potential_water if potential_water > 0 else 0
-            water_collected += potential_water
+        while l < r:
+            if left_max <= right_max:
+                l += 1
+                left_max = max(left_max, nums[l])
+                water_collected += max(0, left_max - nums[l])
+            else:
+                r -= 1
+                right_max = max(right_max, nums[r])
+                water_collected += max(0, right_max - nums[r])
+
 
         return water_collected
-
