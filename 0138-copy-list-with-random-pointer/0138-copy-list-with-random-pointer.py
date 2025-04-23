2cab3b601f18768f3abb5c64205b41c1609d5074
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-22 20:26:47 -0700

    Time: 47 ms (19.47%), Space: 18.7 MB (41.44%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,18 +9,25 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        node_map = defaultdict(Node)
-        node_map[None] = None
-
-        curr = head
-        while curr:
-            node_map[curr] = Node(curr.val)
-            curr = curr.next
-
-        curr = head
-        while curr:
-            node_map[curr].next = node_map[curr.next]
-            node_map[curr].random = node_map[curr.random]
-            curr = curr.next
         
-        return node_map[head]
+        if not head: return None
+
+        # hash-map that maps old node to newly created node
+        listmap = defaultdict(Node)
+
+        # creating the new linked list and inserting the kye-value pairs in our listmap
+        curr = head
+        while curr:
+            newNode = Node(x = curr.val)
+            listmap[curr] = newNode
+            curr = curr.next
+
+        # now we assign the next and the random pointer of the nodes in our new linked list 
+        for oldNode, newNode in listmap.items():
+            if oldNode.next: newNode.next = listmap[oldNode.next]
+            else: newNode.next = None    
+            if oldNode.random: newNode.random = listmap[oldNode.random]
+            else: newNode.random = None
+        
+        return listmap[head]
+        
