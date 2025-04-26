81b3875814759761fe3869780e5265e8b9a4c191
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-25 17:54:51 -0700

    Time: 175 ms (76.07%), Space: 46.1 MB (79.84%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,27 +1,27 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        neighbors = collections.defaultdict(list)
+        
+        neighbors = defaultdict(list)
         for i, j in connections:
             neighbors[i].append(j)
             neighbors[j].append(i)
-        connections = {(i, j) for i, j in connections}
-        
-        def bfs(root):
-            nonlocal neighbors, connections, change
-            q = collections.deque()
-            visit = set()
-            visit.add(root)
-            q.append(root)
-            while q:
-                node = q.popleft() 
-                for n in neighbors[node]:
-                    if n in visit: continue
-                    if (n, node) not in connections: change += 1
-                    visit.add(n)
-                    q.append(n)
-    
 
+        routes = {(i, j) for i, j in connections}
+
+        queue = deque()
+        visited = set()
         change = 0
-        bfs(0)
+
+        queue.append(0)
+        visited.add(0)
+
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for neighbor in neighbors[node]:
+                    if neighbor in visited: continue
+                    visited.add(neighbor)
+                    queue.appendleft(neighbor)
+                    if (neighbor, node) not in routes: change += 1
 
         return change
