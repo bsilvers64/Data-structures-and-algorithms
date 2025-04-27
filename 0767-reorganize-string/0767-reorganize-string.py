b6571d9ac1a7da36e4466ad5acea1efe78705396
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-04-26 21:32:02 -0700

    Time: 0 ms (100%), Space: 18 MB (8.19%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,33 +1,30 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         count = defaultdict(int)
-        for c in s:
-            count[c] += 1
-        
-        max_letter, max_count = '', 0
-        for c, freq in count.items():
-            if freq > max_count:
-                max_letter = c
-                max_count = freq
-        
+        for char in s: count[char] += 1
+        N = len(s)
 
-        if max_count > ((len(s)+1)//2): return ""
+        maxchar, maxchar_count = '', 0
+        for char, freq in count.items():
+            if freq > maxchar_count:
+                maxchar_count = freq
+                maxchar = char
+        
+        if maxchar_count > ((N+1)//2): return ""
 
         result = [''] * len(s)
 
-        index = 0
-
-        while count[max_letter] != 0:
-            result[index] = max_letter
-            count[max_letter] -= 1
-            index += 2
-
-
+        i = 0
+        while count[maxchar]:
+            result[i] = maxchar
+            count[maxchar] -= 1
+            i += 2
+        
         for char, freq in count.items():
             while freq:
-                if index >= len(s): index = 1
-                result[index] = char
+                if i >= N: i = 1
+                result[i] = char
                 freq -= 1
-                index += 2
+                i += 2
 
-        return "".join(result) 
+        return "".join(result)
