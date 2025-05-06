a873fd42867a7921a502315f35555c96163bd420
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-05 19:48:53 -0700

    Time: 0 ms (100%), Space: 18.2 MB (22.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)-1
-        l, r = 0, n
+        # first we find the minimum number
+        l, r = 0, len(nums)-1
+
         while l < r:
             mid = (l+r)//2
-            if nums[mid] > nums[r]: l = mid+1
+            if nums[mid] > nums[r]: l = mid + 1
             else: r = mid
-        min_index = l
-
-        #if min_index == 0: l, r = 0, n
-        if nums[min_index] <= target <= nums[n]:
-            l, r = min_index, n 
+        
+        if nums[l] <= target <= nums[-1]:
+            r = len(nums)-1
         else:
-            l, r = 0, min_index - 1
-
+            r = l - 1
+            l = 0
+        
+        
         while l <= r:
             mid = (l+r)//2
             if nums[mid] == target: return mid
-            elif nums[mid] < target: l = mid + 1
-            else: r = mid - 1 
-
-        return -1           
+            elif nums[mid] > target: r = mid - 1
+            else: l = mid + 1
+        
+        return -1
