a8f32ccfb4a4ae2f4b2751ef4cc63efafbfc324e
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-06 16:57:03 -0700

    Time: 135 ms (65.91%), Space: 39.7 MB (34.37%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,35 +1,37 @@
 from heapq import heappush, heappop
+
 class MedianFinder:
 
     def __init__(self):
-        # large - min-heap and small - max-heap
-        self.large = []
-        self.small = []
-
+        self.small = []     # this is the max-heap
+        self.large = []     # this is the min-heap
+        
 
     def addNum(self, num: int) -> None:
-        # default push to max-heap
         heappush(self.small, -num)
 
-        # make sure every num in max-heap (small) is smaller than every num in min-heap (larger)
-        if (self.large and self.small and (self.small[0] * -1) > self.large[0]):
-            val =  -1 * heappop(self.small)
-            heappush(self.large, val)
+        # we make sure that every element of the min heap is smaller
+        # than every element of the max heap
+
+        if self.large and self.large[0] < (-self.small[0]):
+            temp = -heappop(self.small)
+            heappush(self.large, temp)
         
-        # make sure length of both heaps are about the same
-        if len(self.large) > len(self.small) + 1:
-            val = heappop(self.large)
-            heappush(self.small, -val)
-        
+        # now we make sure that the heaps are of almost the same size +-1
         if len(self.small) > len(self.large) + 1:
-            val = -1 * heappop(self.small)
-            heappush(self.large, val)
-        
+            temp = -heappop(self.small)
+            heappush(self.large, temp)
+        elif len(self.large) > len(self.small) + 1:
+            temp = heappop(self.large)
+            heappush(self.small, -temp)
 
     def findMedian(self) -> float:
-        if len(self.small) == len(self.large): return (-self.small[0] + self.large[0]) / 2
-        elif len(self.small) > len(self.large): return -self.small[0]
-        else: return self.large[0]
+        if len(self.small) == len(self.large):
+            return (-self.small[0] + self.large[0])/2
+        elif len(self.small) > len(self.large):
+            return -self.small[0]
+        else:
+            return self.large[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
