d04c47ff82d7786d7076da1725ddb40fcb2e34e0
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-07 20:12:23 -0700

    Time: 0 ms (100%), Space: 17.8 MB (71.8%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,9 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # index from the last of the result array
         index = m+n-1
-        i, j = m-1, n-1
 
+        i = m-1
+        j = n-1
+
+        # we fill the last, whichever element is larger and move from last to first
         while i >= 0 and j >= 0:
             if nums1[i] >= nums2[j]:
                 nums1[index] = nums1[i]
@@ -15,8 +19,11 @@ class Solution:
                 j -= 1
             index -= 1
         
-        # if num1 elements are over but num2s are left, we put those remaining ones in nums1
-        # otherwise if num2s are over it means, the rest is already sorted.
+        # in case if we finished the nums1 array but nums2 array is still left
+        # that means remaining nums2 elements are smaller so we just fill the 
+        # rest of the array nums1
+
+        # if nums2 finished first, we don't do anything because nums1 is already sorted then
 
         while j >= 0:
             nums1[index] = nums2[j]
