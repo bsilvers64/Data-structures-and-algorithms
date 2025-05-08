94968c2b05e45cb7944f883b2b61fa81fd03c31f
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-07 20:38:33 -0700

    Time: 0 ms (100%), Space: 17.7 MB (78.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp1, dp2 = 1, 1
-        curr = 1
-
-        while n-1:
-            curr = dp1 + dp2
-            dp2 = dp1
-            dp1 = curr
-            n -= 1
+        dp0, dp1 = 1, 1
         
+        for _ in range(n-1):
+            temp = dp1 + dp0
+            dp0 = dp1
+            dp1 = temp
+            
         return dp1
