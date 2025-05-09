5701bbae4fefff1eb746240400eaf077750fa615
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-08 18:42:21 -0700

    Time: 7 ms (47.72%), Space: 20 MB (8.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        for course, preq in prerequisites:
-            graph[course].append(preq)
+
+        for course, prerequisite in prerequisites:
+            graph[course].append(prerequisite)
+        
+        """ return if there exists a cycle in the graph starting 
+            traversal from node """
+        def dfs(node) -> bool:
+            if node in visited: return False
+            if node in visiting: return True
+
+            visiting.add(node)
+
+            for parent in graph[node]:
+                if dfs(parent): return True
+
+            visiting.remove(node)
+            visited.add(node)
+
+            return False
+
         
         visited = set()
         visiting = set()
 
-        def dfs(node):
-            if node in visited:
-                return False
-            if node in visiting:
-                return True
-            
-            visiting.add(node)
-            for prereq in graph[node]:
-                if dfs(prereq): return True
-            
-            visiting.remove(node)
-            visited.add(node)
-            return False
-        
         for course in range(numCourses):
-            if course not in visited and dfs(course): return False
+            if course not in visited:
+                if dfs(course): return False
         
         return True
