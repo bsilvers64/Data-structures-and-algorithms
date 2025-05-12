3b79c0762e9d713a4dff69c3f1c9dae0133133af
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-11 19:42:14 -0700

    Time: 167 ms (89.62%), Space: 21.5 MB (76.08%) - LeetHub

--- 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.py
+++ 3434-maximum-frequency-after-subarray-operation/3434-maximum-frequency-after-subarray-operation.py
@@ -1,20 +1,10 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        count = Counter(nums)
-
-        def max_boost(b):
-            curr, res = 0, 0
-            for num in nums:
-                if num == k: curr -= 1
-                if num == b: curr += 1
-                if curr < 0: curr = 0
-                res = max(res, curr)
-            return res
-
         res = 0
-        for num in count: 
-            res = max(res, max_boost(num))
-        
-        return count[k] + res
+        count = defaultdict(int)
 
+        for num in nums:
+            count[num] = max(count[num], count[k]) + 1
+            res = max(res, count[num] - count[k])
         
+        return res + count[k]
