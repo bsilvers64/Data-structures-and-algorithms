d97b59a7f06fa84f10e280345221f00cf63a6d13
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-11 18:13:54 -0700

    Time: 31 ms (20.2%), Space: 18 MB (34.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,30 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        # tracks the count of each character appearing in the string
-        character_frequency = defaultdict(int)
+        count = defaultdict(int)
+        result = 0
 
-        l = 0
-        r = 0
-
-        max_length = 0
-
+        l, r = 0, 0
         while r < len(s):
-
-            # increase the current character's count
-            character_frequency[s[r]] += 1
-
-            # if the window size is greater than the number of elements in our map
-            # it means than we have duplicate elements, so we shrink our window
-            while (r-l+1) > len(character_frequency) :
-                character_frequency[s[l]] -= 1
-                if character_frequency[s[l]] == 0:
-                    del character_frequency[s[l]]
+            count[s[r]] += 1
+            while len(count) != (r-l+1):
+                count[s[l]] -= 1
+                if count[s[l]] == 0: 
+                    del count[s[l]]
                 l += 1
-
-            # track max length of the substring
-            max_length = max(max_length, r-l+1)
+            result = max(result, r-l+1)
             r += 1
 
-        return max_length
-
+        return result
