175193865f5196ace7bbf8a1532b6cba72763554
Bhavansh sthapak <48654366+bsilvers64@users.noreply.github.com>
2025-05-11 20:21:04 -0700

    Time: 144 ms (56.16%), Space: 33.4 MB (61.5%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.py
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.py
@@ -1,40 +1,46 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        odd_freq, even_freq = defaultdict(int), defaultdict(int)
+        odd, even = defaultdict(int), defaultdict(int)
+        N = len(nums)
 
-        for i in range(len(nums)):
-            if (i%2==0): even_freq[nums[i]] += 1
-            else: odd_freq[nums[i]] += 1
+        for i in range(N):
+            if i % 2 == 0:
+                even[nums[i]] += 1
+            else:
+                odd[nums[i]] += 1
+            
+        odd_max, even_max = 0, 0
+        odd_max_count, even_max_count = 0, 0
 
-        max_odd, max_odd_freq = 0, 0
-        max_even, max_even_freq = 0, 0
+        for num, count in odd.items():
+            if count > odd_max_count: 
+                odd_max_count = count
+                odd_max = num
 
-        for num, count in odd_freq.items():
-            if count > max_odd_freq:
-                max_odd_freq = count
-                max_odd = num
+        for num, count in even.items():
+            if count > even_max_count: 
+                even_max_count = count
+                even_max = num
 
-        for num, count in even_freq.items():
-            if count > max_even_freq:
-                max_even_freq = count
-                max_even = num
 
-        second_max_odd, second_max_odd_freq = 0, 0
-        second_max_even, second_max_even_freq = 0, 0       
+        odd_max_2, even_max_2 = 0, 0
+        odd_max_2_count, even_max_2_count = 0, 0
 
-        for num, count in odd_freq.items():
-            if count > second_max_odd_freq and num != max_odd:
-                second_max_odd_freq = count
-                second_max_odd = num
 
-        for num, count in even_freq.items():
-            if count > second_max_even_freq and num != max_even:
-                second_max_even_freq = count
-                second_max_even = num
+        for num, count in odd.items():
+            if count > odd_max_2_count and odd_max != num: 
+                odd_max_2_count = count
+                odd_max_2 = num
+
+        for num, count in even.items():
+            if count > even_max_2_count and even_max != num: 
+                even_max_2_count = count
+                even_max_2 = num
         
-        if max_odd != max_even: return len(nums) - max_odd_freq - max_even_freq
+        if odd_max != even_max:
+            return N - odd_max_count - even_max_count
         else:
             return min(
-                len(nums) - max_odd_freq - second_max_even_freq,
-                len(nums) - max_even_freq - second_max_odd_freq
+                N - odd_max_count - even_max_2_count,
+                N - even_max_count - odd_max_2_count
             )
